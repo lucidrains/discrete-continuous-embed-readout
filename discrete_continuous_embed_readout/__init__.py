f8c3da130f2549710d271c7bdbeac3a57cdaae72
lucidrains <lucidrains@gmail.com>
2025-12-11 07:33:14 -0800

    outline

---
+++ discrete_continuous_embed_readout/__init__.py
@@ -0,0 +1,4 @@
+from discrete_continuous_embed_readout.discrete_continuous_embed_readout import (
+    Embed,
+    Readout
+)
--- discrete_continuous_embed_readout/discrete_continuous_embed_readout.py
+++ discrete_continuous_embed_readout/discrete_continuous_embed_readout.py
@@ -1,3 +1,38 @@
+from __future__ import annotations
 
 import torch
+import torch.nn.functional as F
 from torch.nn import Module, ModuleList
+
+from torch.distributions import Normal
+
+# helpers
+
+def exists(v):
+    return v is not None
+
+def default(v, d):
+    return v if exists(v) else d
+
+# tensor helpers
+
+def log(t, eps = 1e-20):
+    return t.clamp_min(eps).log()
+
+# classes
+
+class Embed(Module):
+    def __init__(
+        self,
+        dim
+    ):
+        super().__init__()
+        self.dim = dim
+
+class Readout(Module):
+    def __init__(
+        self,
+        dim
+    ):
+        super().__init__()
+        self.dim = dim
