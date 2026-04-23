65acd9d99e8e25adc976dcb960cb62adf914b23c
lucidrains <lucidrains@gmail.com>
2026-04-23 06:38:17 -0700

    export

--- discrete_continuous_embed_readout/__init__.py
+++ discrete_continuous_embed_readout/__init__.py
@@ -2,5 +2,6 @@ from discrete_continuous_embed_readout.discrete_continuous_embed_readout import 
     Embed,
     Readout,
     EmbedAndReadout,
-    MultiCategorical
+    MultiCategorical,
+    ParameterlessReadout
 )
